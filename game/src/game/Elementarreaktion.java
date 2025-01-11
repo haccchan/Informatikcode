b9c17ee5208b73318828dcf7396ba2687cc40de4
haccchan~ <ptht0711@gmail.com>
2025-01-11 19:03:36 +0100

    .

--- game/src/game/Elementarreaktion.java
+++ game/src/game/Elementarreaktion.java
@@ -1,52 +1,39 @@
 package game;
 
 public class Elementarreaktion {
-    String[] team;
-    boolean wasser = false; 
-    boolean erde = false;
-    boolean luft = false;
-    boolean feuer = false;
-    
-    boolean sprießen = false;
-    boolean verdampfen = false;
-    boolean beben = false;
-    boolean brennen = false;
-    boolean tropfen = false;
-    boolean uberladen = false;
-    
-    public Elementarreaktion() {
-        team = new String[0];
-    }
-
-    // Thêm element vào team
-    public void Elementhinzufugen(String element) {
-        String[] neuteam = new String[team.length + 1];
-        System.arraycopy(team, 0, neuteam, 0, team.length);
-        neuteam[team.length] = element;
-        team = neuteam;
-    }
-
-    // Loại bỏ element đầu tiên khỏi team
-    public void Elemententfernen() {
-        if (team.length > 0) { 
-            String[] neuteam = new String[team.length - 1]; 
-            System.arraycopy(team, 1, neuteam, 0, team.length - 1);
-            team = neuteam; 
-        }
-    }
-
-    // Kiểm tra các phản ứng nguyên tố
-    public void Elementprufen() {
-        // Khởi tạo lại các phản ứng nguyên tố mỗi lần kiểm tra
-        sprießen = false;
-        verdampfen = false;
-        beben = false;
-        brennen = false;
-        tropfen = false;
-        uberladen = false;
-
-        // Kiểm tra các yếu tố có trong team
-        for (int i = 0; i < team.length; i++) {
+	String[] team;
+	boolean wasser = false; 
+	boolean erde = false;
+	boolean luft = false;
+	boolean feuer = false;
+	
+	boolean sprießen = false;
+	boolean verdampfen = false;
+	boolean beben = false;
+	boolean brennen = false;
+	boolean tropfen = false;
+	boolean uberladen = false;
+	
+	public Elementarreaktion() {
+		team = new String[0];
+	}
+	public void Elementhinzufugen(String element) {
+	        String[] neuteam = new String[team.length + 1];
+	        for (int i = 0; i < team.length; i++) {
+	            neuteam[i] = team[i];
+	        }
+	        neuteam[team.length] = element;
+	        team = neuteam;
+	 }
+	public void Elemententfernen() {
+	    if (team.length == 3) { 
+	        String[] neuteam = new String[1]; 
+	        neuteam[0] = team[0];
+	        team = neuteam; 
+	    }
+	}
+	public void Elementprufen() {
+		for (int i = 0; i < team.length; i++) {
             String element = team[i];
             if (element.equals("Wasser")) {
                 wasser = true;
@@ -58,43 +45,12 @@ public class Elementarreaktion {
                 luft = true;
             }
         }
+		boolean sprießen = wasser && luft;
+		boolean beben = luft && erde;
+		boolean brennen = luft && feuer;
+		boolean verdampfen = wasser && feuer;
+		boolean tropfen = wasser && erde;
+		boolean uberladen = feuer && erde;
+	}
 
-        // Kiểm tra các phản ứng nguyên tố
-        sprießen = wasser && luft;
-        beben = luft && erde;
-        brennen = luft && feuer;
-        verdampfen = wasser && feuer;
-        tropfen = wasser && erde;
-        uberladen = feuer && erde;
-    }
-
-    // Getter cho các phản ứng nguyên tố
-    public boolean isSprießen() {
-        return sprießen;
-    }
-
-    public boolean isVerdampfen() {
-        return verdampfen;
-    }
-
-    public boolean isBeben() {
-        return beben;
-    }
-
-    public boolean isBrennen() {
-        return brennen;
-    }
-
-    public boolean isTropfen() {
-        return tropfen;
-    }
-
-    public boolean isUberladen() {
-        return uberladen;
-    }
-
-    // Getter cho team
-    public String[] getTeam() {
-        return team;
-    }
 }
